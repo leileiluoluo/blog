30d4023dd888d4537673066142fff2c30deba433
olzhy <olzhy@qq.com>
2020-04-03 10:01:28 +0800

    custom js

---
+++ static/js/custom.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">var cnzz_protocol = " https://";document.write(unescape("%3Cspan id='cnzz_stat_icon_1261214542'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/stat.php%3Fid%3D1261214542%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
