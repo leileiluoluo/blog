f12fa2ada93948a6ab79e6ba664151aa5d4b6010
yanleil <yanleil@cn.ibm.com>
2020-08-03 08:43:11 +0800

    search loading

--- themes/northendlab-hugo/static/plugins/search/search.js
+++ themes/northendlab-hugo/static/plugins/search/search.js
@@ -32,7 +32,7 @@ function showLoading() {
 }
 
 function executeSearch(searchQuery) {
-  var _LoadingHtml = '<div id="loadingDiv" style="display: none; "><div id="over" style=" position: absolute;top: 0;left: 0; width: 100%;height: 100%; background-color: #f5f5f5;opacity:0.5;z-index: 1000;"></div><div id="layout" style="position: absolute;top: 40%; left: 40%;width: 20%; height: 20%;  z-index: 1001;text-align:center;"><img src="/static/images/site/loading.gif" /></div></div>';
+  var _LoadingHtml = '<div id="loadingDiv" style="display: none; "><div id="over" style=" position: absolute;top: 0;left: 0; width: 0%;height: 10%; background-color: #f5f5f5;opacity:0.5;z-index: 1000;"></div><div id="layout" style="position: absolute;top: 40%; left: 40%;width: 6%; height: 6%;  z-index: 1001;text-align:center;"><img src="/static/images/site/loading.gif" /></div></div>';
   $('#search-results').append(_LoadingHtml);
 
   showLoading();
